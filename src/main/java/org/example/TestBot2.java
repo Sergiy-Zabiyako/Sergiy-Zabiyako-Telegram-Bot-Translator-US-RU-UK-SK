2cf5fdfc215be68009e1c8a9c6ebc1352e035937
Sergiy-Zabiyako <sergiy.zabiyako@gmail.com>
2022-12-04 13:12:44 -0500

    Second commit

--- src/main/java/org/example/TestBot2.java
+++ src/main/java/org/example/TestBot2.java
@@ -6,6 +6,7 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+
 public class TestBot2 extends TelegramLongPollingBot {
     @Override
     public String getBotUsername() {
