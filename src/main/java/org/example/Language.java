d42926651376f5e20429005881adc875761b3e7f
Sergiy-Zabiyako <sergiy.zabiyako@gmail.com>
2022-12-07 17:13:35 -0500

    Commit espaniol

--- src/main/java/org/example/Language.java
+++ src/main/java/org/example/Language.java
@@ -5,6 +5,6 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum Language {
-    EN(1),RU(2),UK(3),SK(4);
+    EN(1),RU(2),UK(3),SK(4),ES(5);
     private final int id;
 }
